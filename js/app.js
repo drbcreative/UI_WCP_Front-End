333189397a8e7fef61c154621a3cc7f424a338fd
Danny <info@dannybcreative.com>
2020-03-31 20:33:27 -0700

    animation added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,1 @@
-$(document).ready(function() {
-  var parallaxBg = $('.parallax-bg');
-  $(window).scroll(function() {
-    var currScrollPos = $(window).scrollTop();
-    parallaxBg.css('background-position', '0' + -currScrollPos / 2 + 'px');
-  });
-});
+
